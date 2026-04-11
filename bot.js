2db20f996e25a9c4f2e1451b121de45fe6f67726
camilocerda-alt <camilo.cerda@alumnos.hispano-americano.cl>
2026-04-11 13:19:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -181,7 +181,7 @@ client.on('interactionCreate', async interaction => {
       embeds: [{
         title: "📱 Redes",
         description:
-          "📺 https://www.youtube.com/Camiteboxea1\n\n" +
+          "📺 https://youtube.com/camiteboxea1?si=KBSNBVAM-nV1T1_W\n\n" +
           "🎮 https://kick.com/camikgg9\n\n" +
           "🎵 https://tiktok.com/@camikgg9",
         color: 0x5865F2
